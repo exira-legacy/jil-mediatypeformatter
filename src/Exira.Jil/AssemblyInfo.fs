d6e195ac9e184733ff62cd90bc804bcad7e0a81a
David Cumps <david@cumps.be>
2015-04-14 10:19:53 +0200

    Add build

---
+++ src/Exira.Jil/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Exira.Jil")>]
+[<assembly: AssemblyProductAttribute("Exira.Jil")>]
+[<assembly: AssemblyDescriptionAttribute("MediaTypeFormatter based on Jil (https://github.com/kevin-montrose/Jil)")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
---
+++ tests/Exira.Jil.Tests/Tests.fs
@@ -0,0 +1,4 @@
+module Exira.Jil.Tests
+
+open Exira.Jil
+open NUnit.Framework
