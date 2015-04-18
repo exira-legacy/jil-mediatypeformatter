2d20b3293e7134c3e43f9f358c4b235a0683d6eb
David Cumps <david@cumps.be>
2015-04-19 01:27:00 +0200

    Bump version to 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,6 @@
+#### 0.0.2 - April 19 2015
+* Target .NET Framework 4.5
+* Depend on FSharp.Core nuget packages
+
 #### 0.0.1 - April 14 2015
 * Initial release
--- src/Exira.Jil/AssemblyInfo.fs
+++ src/Exira.Jil/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.Jil")>]
 [<assembly: AssemblyProductAttribute("Exira.Jil")>]
 [<assembly: AssemblyDescriptionAttribute("MediaTypeFormatter based on Jil (https://github.com/kevin-montrose/Jil)")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
